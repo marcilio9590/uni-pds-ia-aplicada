7a2f632e3f8364d0a7b023a7dd60095adce804fe
Marcilio Cobel <marcilio9590@gmail.com>
2026-03-23 21:53:58 -0300

    api com langchain para conversão de texto para uppercase e lowercase

--- modulo02/exemplo-02/src/graph/graph.ts
+++ modulo02/exemplo-02/src/graph/graph.ts
@@ -3,7 +3,10 @@ import { withLangGraph } from "@langchain/langgraph/zod";
 import { BaseMessage } from "langchain";
 import { z } from "zod/v3";
 import { chatResponse } from "./nodes/chatResponseNode.ts";
+import { fallbackNode } from "./nodes/fallbackNode.ts";
 import { identifyIntent } from "./nodes/identifyIntentNode.ts";
+import { lowerCaseNode } from "./nodes/lowerCaseNode.ts";
+import { upperCaseNode } from "./nodes/upperCaseNode.ts";
 
 const GraphState = z.object({
   messages: withLangGraph(z.custom<BaseMessage[]>(), MessagesZodMeta),
@@ -17,16 +20,39 @@ export function buildGraph() {
   const workflow = new StateGraph({
     stateSchema: GraphState,
   })
-    .addNode("identifyIntenet", identifyIntent)
+    .addNode("identifyIntent", identifyIntent)
+    .addNode("upperCaseNode", upperCaseNode)
+    .addNode("lowerCaseNode", lowerCaseNode)
     .addNode("chatResponse", chatResponse)
+    .addNode("fallbackNode", fallbackNode)
     // .addNode("identifyIntenet", (state, GraphState) => {
     //   return {
     //     ...state,
     //     output: "Teste",
     //   };
     // })
-    .addEdge(START, "identifyIntenet")
-    .addEdge("identifyIntenet", "chatResponse")
+    .addEdge(START, "identifyIntent")
+    .addConditionalEdges(
+      "identifyIntent",
+      (state: GraphState) => {
+        switch (state.command) {
+          case "uppercase":
+            return "uppercase";
+          case "lowercase":
+            return "lowercase";
+          default:
+            return "fallback";
+        }
+      },
+      {
+        uppercase: "upperCaseNode",
+        lowercase: "lowerCaseNode",
+        fallback: "fallbackNode",
+      },
+    )
+    .addEdge("upperCaseNode", "chatResponse")
+    .addEdge("lowerCaseNode", "chatResponse")
+    .addEdge("fallbackNode", "chatResponse")
     .addEdge("chatResponse", END);
   return workflow.compile();
 }
--- modulo02/exemplo-02/tests/router.e2e.test.ts
+++ modulo02/exemplo-02/tests/router.e2e.test.ts
@@ -17,3 +17,36 @@ test("command upper transforms message into UPPERCASE", async () => {
   assert.equal(response.statusCode, 200);
   assert.equal(response.body, expect);
 });
+
+test("command upper transforms message into lowerCase", async () => {
+  const app = createServer();
+  const message = "MAKE THIS MESSAGE LOWER CASE!";
+  const expect = message.toLowerCase();
+  const response = await app.inject({
+    method: "POST",
+    url: "/chat",
+    body: {
+      question: message,
+    },
+  });
+
+  assert.equal(response.statusCode, 200);
+  assert.equal(response.body, expect);
+});
+
+test("command upper transforms message into UNKNOW", async () => {
+  const app = createServer();
+  const message = "Hey There!";
+  const expect =
+    "Unknow command. Try 'make this uppercase' or 'convert to lowercase'";
+  const response = await app.inject({
+    method: "POST",
+    url: "/chat",
+    body: {
+      question: message,
+    },
+  });
+
+  assert.equal(response.statusCode, 200);
+  assert.equal(response.body, expect);
+});
