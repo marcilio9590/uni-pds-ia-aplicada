ecf6fcccc40d32a5a96f21e3bbee6f765e668943
Marcilio Cobel <marcilio9590@gmail.com>
2026-03-30 20:05:15 -0300

    construindo app para recomendação de musicas baseado em preferencia musical

---
+++ modulo02/exemplo-04/src/graph/nodes/chatNode.ts
@@ -0,0 +1,50 @@
+import type { Runtime } from "@langchain/langgraph";
+import { AIMessage, HumanMessage } from "langchain";
+import {
+  ChatResponseSchema,
+  getSystemPrompt,
+  getUserPromptTemplate,
+} from "../../prompts/v1/chatResponse.ts";
+import { OpenRouterService } from "../../services/openrouterService.ts";
+import type { GraphState } from "../graph.ts";
+
+export function createChatNode(llmClient: OpenRouterService) {
+  return async (
+    state: GraphState,
+    runtime?: Runtime,
+  ): Promise<Partial<GraphState>> => {
+    const userContext = "";
+    const systemPromp = getSystemPrompt(userContext);
+    const conversationHistory = state.messages
+      .map((msg) => {
+        `${HumanMessage.isInstance(msg) ? "User" : "AI"}: ${msg.content}`;
+      })
+      .join("\n");
+
+    const userMessage = state.messages.at(-1)?.text as string;
+    const userPrompt = getUserPromptTemplate(userMessage, conversationHistory);
+
+    const result = await llmClient.generateStructured(
+      systemPromp,
+      userPrompt,
+      ChatResponseSchema,
+    );
+
+    if (!result.success || !result.data) {
+      console.error("Falha ao gerar resposta", result.error);
+      return {
+        messages: [
+          new AIMessage("Desculpe, encontrei um erro, Pode tentar novamente?"),
+        ],
+      };
+    }
+    const response = result.data;
+    return {
+      messages: [new AIMessage(response.message)],
+      extractedPreferences: response.shouldSavePreferences
+        ? response.preferences
+        : undefined,
+      needsSummarization: false,
+    };
+  };
+}
